fd196e429d92171b8ca83f30e38086ac8bb83269
Sean Oldham <seanosaur@me.com>
2015-08-20 15:01:19 -0500

    undo config change

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,6 @@ module Newoldham
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.serve_static_assets = true
-
     config.generators do |g|
       g.test_framework :rspec,
       fixtures: true,
